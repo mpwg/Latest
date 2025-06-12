bd26beaf0f9e08f172d02304091b3dc3200896de
Max Langer <maxlanger@me.com>
2025-06-12 22:23:54 +0200

    Cleanup table row header

--- Latest/Interface/Main Window/Update Table View/Views/UpdateGroupRowView.swift
+++ Latest/Interface/Main Window/Update Table View/Views/UpdateGroupRowView.swift
@@ -10,16 +10,6 @@ import Cocoa
 
 /// The row view used by the group rows in the update tableView
 class UpdateGroupRowView: NSTableRowView {
-    
 	// Prevent any row view drawing
 	override func draw(_ dirtyRect: NSRect) {}
-	
-	// Make sure the display of the row is not altered in any way
-	override var isGroupRowStyle: Bool {
-		get {
-			return false
-		}
-		set {}
-	}
-        
 }
